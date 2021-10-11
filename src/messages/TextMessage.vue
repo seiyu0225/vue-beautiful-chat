cfa00c19307641a4dea04bc17c8f748151d0f5c4
seiyu0225 <seiyu0225@gmail.com>
2021-10-11 21:01:38 +0900

    delete other feature

--- src/messages/TextMessage.vue
+++ src/messages/TextMessage.vue
@@ -1,40 +1,14 @@
 <template>
   <div class="sc-message--text" :style="messageColors">
-    <template>
-      <div class="sc-message--toolbox" :style="{background: messageColors.backgroundColor}">
-        <button v-if="showEdition && me && message.id" :disabled="isEditing" @click="edit">
-          <IconBase :color="isEditing ? 'black' : messageColors.color" width="10" icon-name="edit">
-            <IconEdit />
-          </IconBase>
-        </button>
-        <button v-if="showDeletion && me && message.id" @click="$emit('remove')">
-          <IconBase :color="messageColors.color" width="10" icon-name="remove">
-            <IconCross />
-          </IconBase>
-        </button>
-        <slot name="text-message-toolbox" :message="message" :me="me"> </slot>
-      </div>
-    </template>
+    <template> </template>
     <slot :message="message" :messageText="messageText" :messageColors="messageColors" :me="me">
       <p class="sc-message--text-content" v-html="messageText"></p>
-      <p v-if="message.data.meta" class="sc-message--meta" :style="{color: messageColors.color}">
-        {{ message.data.meta }}
-      </p>
-      <p v-if="message.isEdited" class="sc-message--edited">
-        <IconBase width="10" icon-name="edited">
-          <IconEdit />
-        </IconBase>
-        edited
-      </p>
     </slot>
   </div>
 </template>
 
 <script>
 import {mapState} from '../store/'
-import IconBase from './../components/IconBase.vue'
-import IconEdit from './../components/icons/IconEdit.vue'
-import IconCross from './../components/icons/IconCross.vue'
 import escapeGoat from 'escape-goat'
 import Autolinker from 'autolinker'
 import store from '../store/'
@@ -42,11 +16,7 @@ import store from '../store/'
 const fmt = require('msgdown')
 
 export default {
-  components: {
-    IconBase,
-    IconCross,
-    IconEdit
-  },
+  components: {},
   props: {
     message: {
       type: Object,
